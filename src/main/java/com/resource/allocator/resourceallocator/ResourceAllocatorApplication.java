a852321b83e64f73618e6ecf7ad91649920a1108
varadarajan1 <vvaradu@gmail.com>
2019-08-25 11:02:51 +0530

    Add Spring Boot Rest API base project

---
+++ src/main/java/com/resource/allocator/resourceallocator/ResourceAllocatorApplication.java
@@ -0,0 +1,13 @@
+package com.resource.allocator.resourceallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAllocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAllocatorApplication.class, args);
+	}
+
+}
